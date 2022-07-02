fd42ca229241ba0a21e653abaf62bc4eb6d1e86e
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-07-02 14:15:00 +0900

    Update ProgE2Test.java

--- src/test/java/ProgE2Test.java
+++ src/test/java/ProgE2Test.java
@@ -2,7 +2,9 @@ import org.junit.jupiter.api.Test;
 import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * @version (20220616)
+ */
 public class ProgE2Test {
 
     @Test
@@ -17,29 +19,36 @@ public class ProgE2Test {
 
         // action
         ProgE2.main(new String[]{"textfile1.txt"}); // 実行時引数をテストする場合
-
+        // undo the binding in System
+        System.setOut(originalOut);
+        
         // assertion
         String expected = "1.読み込み確認用ファイル\n" +
                 "2.ここは2行目です。\n" +
-                "3.\n";
-        assertTrue(bos.toString().contains(expected));
-
-        // undo the binding in System
-        System.setOut(originalOut);
+                "3.";
+        assertTrue(bos.toString().contains(expected),"ファイルから読み込んだ内容とprintした内容が一致しません!");
     }
 
     @Test
     public void testCatchException()
     {
         PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
+        ByteArrayOutputStream bosOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bosOut));
 
         StandardInputStream in = new StandardInputStream();
         System.setIn(in);
 
         // assertion
-        assertDoesNotThrow(() -> ProgE2.main(new String[]{"src/main/java/abcdefg.txt"}));
-    }
+        assertDoesNotThrow(
+            () -> ProgE2.main(new String[]{"src/main/java/abcdefg.txt"}), 
+            "例外処理をthrowしてはいけません！"
+        );
 
+        // undo the binding in System
+        System.setOut(originalOut);
+        
+        assertTrue(bosOut.toString().contains("ファイル読み込みで例外が発生しました。処理を中断します！"),
+            "読み込みで例が発生したことを示すメッセージ表示がない、またはその内容が指示と異なります!" );
+    }
 }
